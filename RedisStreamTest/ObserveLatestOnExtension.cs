7fe7cf4f1847c622477c2a340a2337b88ee862fb
Tun Tun Win <tuntunw@gmail.com>
2022-04-17 00:03:30 +0630

    Add ObserveLastestOn

---
+++ RedisStreamTest/ObserveLatestOnExtension.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Concurrency;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisStreamTest
+{
+    static class ObserveLatestOnExtension
+    {
+        public static IObservable<T> ObserveLatestOn<T>(this IObservable<T> source, IScheduler scheduler)
+        {
+            return Observable.Create<T>(observer =>
+            {
+                Notification<T> outsideNotification = null;
+                var gate = new object();
+                bool active = false;
+                var cancelable = new MultipleAssignmentDisposable();
+                var disposable = source.Materialize().Subscribe(thisNotification =>
+                {
+                    bool alreadyActive;
+                    lock (gate)
+                    {
+                        alreadyActive = active;
+                        active = true;
+                        outsideNotification = thisNotification;
+                    }
+
+                    if (!alreadyActive)
+                    {
+                        cancelable.Disposable = scheduler.Schedule(self =>
+                        {
+                            Notification<T> localNotification = null;
+                            lock (gate)
+                            {
+                                localNotification = outsideNotification;
+                                outsideNotification = null;
+                            }
+                            localNotification.Accept(observer);
+                            bool hasPendingNotification = false;
+                            lock (gate)
+                            {
+                                hasPendingNotification = active = (outsideNotification != null);
+                            }
+                            if (hasPendingNotification)
+                            {
+                                self();
+                            }
+                        });
+                    }
+                });
+                return new CompositeDisposable(disposable, cancelable);
+            });
+        }
+        public static IObservable<TSource> ObserveLatestOn2<TSource>(
+    this IObservable<TSource> source,
+    IScheduler scheduler)
+        {
+            return Observable.Create<TSource>(observer =>
+            {
+                Notification<TSource> pendingNotification = null;
+                var cancelable = new MultipleAssignmentDisposable();
+
+                var sourceSubscription = source.Materialize()
+                    .Subscribe(notification =>
+                    {
+                        var previousNotification = Interlocked.Exchange(
+                            ref pendingNotification, notification);
+
+                        if (previousNotification != null) return;
+
+                        cancelable.Disposable = scheduler.Schedule(() =>
+                        {
+                            var notificationToSend = Interlocked.Exchange(
+                        ref pendingNotification, null);
+                            notificationToSend.Accept(observer);
+                        });
+                    });
+                return new CompositeDisposable(sourceSubscription, cancelable);
+            });
+        }
+    }
+}
--- RedisStreamTest/Program.cs
+++ RedisStreamTest/Program.cs
@@ -1,8 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using RedisStreamTest;
 using StackExchange.Redis;
 using System.Reactive.Concurrency;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
+using System.Reactive.Subjects;
 
 var connectionMultiplexer = await ConnectionMultiplexer.ConnectAsync("localhost");
 var redisDb = connectionMultiplexer.GetDatabase(0);
@@ -10,29 +12,71 @@ var redisDb = connectionMultiplexer.GetDatabase(0);
 var tokenSource = new CancellationTokenSource();
 var taskKind = Environment.GetCommandLineArgs()[1];
 IDisposable task = null;
-if (taskKind == "polling") {
-    task = CreateTaskFromStreamWithPolling(connectionMultiplexer, redisDb, "log", tokenSource.Token).Subscribe(x =>
-    {
-        Console.WriteLine(x);
-    });
-}
-else {
-    task = CreateTaskFromStreamWithPubSub(connectionMultiplexer, redisDb, "log").Subscribe(x =>
-    {
-        Console.WriteLine(x);
-    });
-}
+
+task = CreateTaskFromStreamWithPubSub(connectionMultiplexer, redisDb, "log").Subscribe(x =>
+{
+    Console.WriteLine(x);
+});
+
 using (task)
     {
     using (RunPublisher(redisDb, connectionMultiplexer.GetSubscriber()))
     {
         Console.ReadLine();
     }
-    //Console.ReadLine();
 }
     
-static IDisposable RunPublisher(IDatabase redisDb, ISubscriber publisher) {
-    return Observable.Interval(TimeSpan.FromSeconds(2)).Subscribe(x => {
+static IObservable<string> CreateTaskFromStreamWithPubSub(ConnectionMultiplexer connection, IDatabase redisDb, string channel)
+{
+    var lastReadMessage = "0-0";
+    var lastReadMessageData = redisDb.StringGet($"{channel}:lastReadMessage", CommandFlags.None);
+    if (string.IsNullOrEmpty(lastReadMessageData))
+    {
+        redisDb.StringGetSet($"{channel}:lastReadMessage", lastReadMessage);
+    }
+    else
+    {
+        lastReadMessage = lastReadMessageData;
+    }
+    var subscriber = connection.GetSubscriber();
+
+    var pubsubObs = Observable.Create<string>(obsPubsub => {
+        subscriber.Subscribe($"{channel}:notify", (ch, msg) =>
+        {
+            obsPubsub.OnNext(DateTime.Now.ToString());
+        });
+        return Disposable.Create(() => subscriber.Unsubscribe(channel));
+    });
+    
+    var readStreamObs = Observable.Create<string>(obsReadstream => {
+        
+        return pubsubObs
+         .ObserveLatestOn(Scheduler.CurrentThread)
+        .Subscribe(msg =>
+        {
+            Task.Run(async () =>
+            {
+                var messages = await redisDb.StreamReadAsync(channel, lastReadMessage);
+
+                foreach (var message in messages)
+                {
+                    obsReadstream.OnNext($"{message.Id} -> {message.Values[0].Name}: {message.Values[0].Value} / {message.Values[1].Name}: {message.Values[1].Value}");
+                    lastReadMessage = message.Id;
+                }
+                Console.WriteLine($"Total Message {messages.Length}");
+                await Task.Delay(TimeSpan.FromSeconds(3));
+                redisDb.StringGetSet($"{channel}:lastReadMessage", lastReadMessage);
+            }).Wait();
+            
+        });
+    });
+    return readStreamObs;
+}
+
+
+static IDisposable RunPublisher(IDatabase redisDb, ISubscriber publisher)
+{
+    return Observable.Interval(TimeSpan.FromSeconds(1)).Subscribe(x => {
         var value = new NameValueEntry[]
               {
                     new NameValueEntry("id", Guid.NewGuid().ToString()),
@@ -45,89 +89,3 @@ static IDisposable RunPublisher(IDatabase redisDb, ISubscriber publisher) {
 
 }
 
-
-static IObservable<string> CreateTaskFromStreamWithPubSub(ConnectionMultiplexer connection, IDatabase redisDb, string channel)
-{
-    var lastReadMessage = "0-0";
-    SemaphoreSlim taskFromStreamBlocker = new SemaphoreSlim(1);
-    var lastReadMessageData = redisDb.StringGet($"{channel}:lastReadMessage", CommandFlags.None);
-    if (string.IsNullOrEmpty(lastReadMessageData))
-    {
-        redisDb.StringGetSet($"{channel}:lastReadMessage", lastReadMessage);
-    }
-    else
-    {
-        lastReadMessage = lastReadMessageData;
-    }
-
-
-    return Observable.Create<string>(obs =>
-    {
-        var subscriber = connection.GetSubscriber();
-        subscriber.Subscribe($"{channel}:notify", async (ch, msg) =>
-        {
-            var locker = await taskFromStreamBlocker
-                .WaitAsync(0)
-                .ConfigureAwait(false);
-
-            if (!locker)
-            {
-                return;
-            }
-
-            var messages = await redisDb.StreamReadAsync(channel, lastReadMessage);
-
-            foreach (var message in messages)
-            {
-                obs.OnNext($"{message.Id} -> {message.Values[0].Name}: {message.Values[0].Value} / {message.Values[1].Name}: {message.Values[1].Value}");
-                lastReadMessage = message.Id;
-            }
-
-            //redisDb.KeyDelete($"{channel}:lastReadMessage");
-            redisDb.StringGetSet($"{channel}:lastReadMessage", lastReadMessage);
-
-            taskFromStreamBlocker.Release();
-        });
-
-        return Disposable.Create(() => subscriber.Unsubscribe(channel));
-    });
-}
-
-static IObservable<string> CreateTaskFromStreamWithPolling(ConnectionMultiplexer connection, IDatabase redisDb, string channel, CancellationToken cancellationToken)
-{
-    var lastReadMessage = "0-0";
-
-    var lastReadMessageData = redisDb.StringGet($"{channel}:lastReadMessage", CommandFlags.None);
-    if (string.IsNullOrEmpty(lastReadMessageData))
-    {
-        redisDb.StringGetSet($"{channel}:lastReadMessage", lastReadMessage);
-    }
-    else
-    {
-        lastReadMessage = lastReadMessageData;
-    }
-
-    var instance = ThreadPoolScheduler.Instance;
-
-    return Observable.Create<string>(obs =>
-    {
-        var disposable = Observable
-            .Interval(TimeSpan.FromMilliseconds(200), instance)
-            .Subscribe(async _ =>
-            {
-                var messages = await redisDb.StreamReadAsync(channel, lastReadMessage);
-
-                foreach (var message in messages)
-                {
-                    obs.OnNext($"{message.Id} -> {message.Values[0].Name}: {message.Values[0].Value} / {message.Values[1].Name}: {message.Values[1].Value}");
-                    lastReadMessage = message.Id;
-                }
-                Console.WriteLine($"{DateTime.Now} {messages.Length} message received");
-                //redisDb.KeyDelete($"{channel}:lastReadMessage");
-                redisDb.StringGetSet($"{channel}:lastReadMessage", lastReadMessage);
-            });
-        cancellationToken.Register(() => disposable.Dispose());
-
-        return Disposable.Empty;
-    });
-}
