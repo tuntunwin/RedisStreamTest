33a7de1911c035b41e2b8d44529255dd2663f077
Tun Tun Win <tuntunw@gmail.com>
2022-04-17 14:51:59 +0630

    Add Http2WebAppTest project

---
+++ Http2Test/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+
+HttpClient myHttpClient = new HttpClient
+    {
+        DefaultRequestVersion = HttpVersion.Version20,
+        DefaultVersionPolicy = HttpVersionPolicy.RequestVersionOrLower
+    };
+
+    string requestUrl = "https://http2.akamai.com/demo";
+
+    try
+    {
+        Console.WriteLine($"GET {requestUrl}.");
+
+        HttpResponseMessage response = await myHttpClient.GetAsync(requestUrl);
+        response.EnsureSuccessStatusCode();
+
+        Console.WriteLine($"Response HttpVersion: {response.Version}");
+
+        string responseBody = await response.Content.ReadAsStringAsync();
+        Console.WriteLine($"Response Body Length is: {responseBody.Length}");
+        Console.WriteLine($"------------Response Body------------");
+        Console.WriteLine(responseBody);
+        Console.WriteLine($"------------End of Response Body------------");
+
+    }
+    catch (HttpRequestException e)
+    {
+        Console.WriteLine($"HttpRequestException : {e.Message}");
+    }
+
+    Console.WriteLine($"Press Enter to exit....");
+    Console.ReadLine();
---
+++ Http2WebApp3/Program.cs
@@ -0,0 +1,34 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "MyAllowSpecificOrigins",
+        policy =>
+        {
+            policy.AllowAnyOrigin();
+        });
+});
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseCors("MyAllowSpecificOrigins");
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action=Index}/{id?}");
+
+app.MapFallbackToFile("index.html"); ;
+
+app.Run();
